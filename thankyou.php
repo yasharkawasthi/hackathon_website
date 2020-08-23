6978331219bb43407d3a41e1db29ba3602660ff1
Yashark Awasthi <yasharkawasthi@gmail.com>
2020-08-23 16:38:00 +0530

    completed

--- thankyou.php
+++ thankyou.php
@@ -1,31 +1,24 @@
 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hackathon | Thank You</title>
-    <link rel="stylesheet" href="css/stylethank.css">
+    <title>Thank You</title>
+    <link rel="stylesheet" href="css/material-design-iconic-font.min.css">
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 </head>
-<body>
-    <section class="page">
-
-        <br><br><br>
-        <div class="heading">
-            <h1 class="thanks">Thank You for Registering!</h1>    
-        </div>
-
-        <div class="image">
-            <img src="assets/check.png" alt="" >
-        </div>
-        
-        <br><br><br>
-        <div class="para">
-            <p class="text">Your Submission is received. Version Team wishes you all the best for the subsequent rounds.
-            </p>
-        </div>
-    </section>
+<body onload='popUp()'>
+    <script>
+        function popUp(){
+            swal({
+                title: "Thank You!",
+                text: "Your response have been saved successfully!",
+                icon: "success",
+                button: "OK",
+                });  
+        }
+    </script>
 </body>
-</html>
+</html> 
